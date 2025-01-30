4dfd9606cca24574e39e3f200aef79a3752f1a54
j-cop-k <mr.jackob@interia.pl>
2025-01-30 18:06:43 +0100

    kempa naprawil zjebany stepzen

--- FoodDiary/src/app/_layout.tsx
+++ FoodDiary/src/app/_layout.tsx
@@ -7,20 +7,20 @@ import {
 } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://oneonta.stepzen.net/api/belligerent-waterbuffalo/__graphql',
+  uri: 'https://arrahmani.eu-central-a.ibm.stepzen.net/api/hasty-hare/__graphql',
   cache: new InMemoryCache(),
   headers: {
     Authorization:
-            'apikey arrahmani::local.net+1000::4428fe428bbf593a38d25d08ec706221324f69ebead78291d28e1875c0ed3f9e'
-        },
-    });
-    
-    const RootLayout = () => {
-      return (
-        <ApolloProvider client={client}>
-          <Stack />
-        </ApolloProvider>
-      );
-    };
-    
-    export default RootLayout;
+      'apikey arrahmani::local.net+1000::4428fe428bbf593a38d25d08ec706221324f69ebead78291d28e1875c0ed3f9e',
+  },
+});
+
+const RootLayout = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Stack />
+    </ApolloProvider>
+  );
+};
+
+export default RootLayout;
